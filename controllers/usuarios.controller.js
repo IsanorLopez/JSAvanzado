86bbfc07e80a14b9484521ab8b657063920ae7b9
IsanorLopez <isanoralejandro@gmail.com>
2022-09-29 12:02:43 -0600

    JSAvanzado[ - Feat: Definicion de controller ]

---
+++ controllers/usuarios.controller.js
@@ -0,0 +1,59 @@
+const { request, response } = require('express');
+const axios = require('axios')
+
+const usuariosGet = function (req, res = response) {
+    res.json({
+        msg: 'Get Api controller',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPost = function (req = request, res = response) {
+    
+    const {nombre, edad} = req.body;
+    
+    res.json({
+        msg: 'Post Api controller',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = function (req = request, res = response) {
+    
+    const { id } = req.params;
+    
+    res.json({
+        msg: 'Put Api controller',
+        id
+    });
+}
+
+const usuariosPatch = function (req = request, res = response) {
+    
+    const { id } = req.params;
+    
+    res.json({
+        msg: 'Patch Api controller',
+        id
+    });
+}
+
+const usuariosDelete = function (req = request, res = response) {
+
+    const query = req.query;
+
+    res.json({
+        msg: 'Delete Api controller',
+        query
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
