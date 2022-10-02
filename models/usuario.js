c27249cac336c64d73ecd41b1bba8062c0e073d9
IsanorLopez <isanoralejandro@gmail.com>
2022-10-02 13:04:34 -0600

    JSAvanzado[ - Feat: Definicio del modelo de usuarios ]

---
+++ models/usuario.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+
+module.exports = model('Usuario', UsuarioSchema);
